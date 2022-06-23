c273042f848e6f154bee92ac9583a3da3ae954b9
Bryce Hostetler <bhostetler@ufl.edu>
2022-06-23 13:02:04 -0400

    Camera preview on imaging tab

--- app/src/main/java/com/uf/automoth/ui/imaging/ImagingFragment.kt
+++ app/src/main/java/com/uf/automoth/ui/imaging/ImagingFragment.kt
@@ -1,12 +1,22 @@
 package com.uf.automoth.ui.imaging
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.uf.automoth.R
 import com.uf.automoth.databinding.FragmentImagingBinding
 
 class ImagingFragment : Fragment() {
@@ -28,15 +38,76 @@ class ImagingFragment : Fragment() {
         _binding = FragmentImagingBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        imagingViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        requestPermissionsIfNecessary()
+
         return root
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.cameraPreview.surfaceProvider)
+                }
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview
+                )
+            } catch (exc: Exception) {
+                Log.e("Camera", "Use case binding failed", exc)
+            }
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
+
+    private fun requestPermissionsIfNecessary() {
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            val permissionLauncher = registerForActivityResult(
+                ActivityResultContracts.RequestMultiplePermissions()
+            ) { isGranted ->
+                if (isGranted.values.all { it }) {
+                    startCamera()
+                } else {
+                    warnPermissionsDenied()
+                }
+            }
+            permissionLauncher.launch(REQUIRED_PERMISSIONS)
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun warnPermissionsDenied() {
+        Toast.makeText(
+            requireContext(), R.string.permissions_denied_toast, Toast.LENGTH_SHORT
+        ).show()
+        // TODO: more persistent warning, maybe mention going to settings
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object {
+        private val REQUIRED_PERMISSIONS =
+            mutableListOf(
+                Manifest.permission.CAMERA,
+            ).apply {
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                    add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                }
+            }.toTypedArray()
     }
 }
--- app/src/main/java/com/uf/automoth/ui/imaging/ImagingViewModel.kt
+++ app/src/main/java/com/uf/automoth/ui/imaging/ImagingViewModel.kt
@@ -1,13 +1,6 @@
 package com.uf.automoth.ui.imaging
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ImagingViewModel : ViewModel() {
-
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is the imaging Fragment"
-    }
-    val text: LiveData<String> = _text
 }
