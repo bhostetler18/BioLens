b27facc1723348e34b91df3ee4f03e19b61f03eb
Bryce Hostetler <bhostetler@ufl.edu>
2022-08-01 13:14:40 -0400

    Add preliminary splash screen

--- app/src/main/java/com/uf/automoth/MainActivity.kt
+++ app/src/main/java/com/uf/automoth/MainActivity.kt
@@ -2,6 +2,7 @@ package com.uf.automoth
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.isVisible
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -16,7 +17,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splash = installSplashScreen()
+
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.mainToolbar)
