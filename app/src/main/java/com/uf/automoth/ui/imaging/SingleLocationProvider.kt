dddf2aca48e46cd1dd095b1e5f6b4265160ee672
Bryce Hostetler <bhostetler@ufl.edu>
2022-08-15 12:45:12 -0400

    Temporary fix for some issues that hadn't appeared before because the Android Studio code inspection was broken

--- app/src/main/java/com/uf/automoth/ui/imaging/SingleLocationProvider.kt
+++ app/src/main/java/com/uf/automoth/ui/imaging/SingleLocationProvider.kt
@@ -1,9 +1,13 @@
 package com.uf.automoth.ui.imaging
 
+import android.Manifest.permission.ACCESS_FINE_LOCATION
 import android.content.Context
+import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationManager
+import android.os.Build
 import android.util.Log
+import androidx.core.content.ContextCompat
 import androidx.core.location.LocationManagerCompat
 import java.util.concurrent.Executors
 import java.util.function.Consumer
@@ -15,31 +19,20 @@ class SingleLocationProvider(context: Context) {
         locationManager = context.applicationContext.getSystemService(Context.LOCATION_SERVICE) as LocationManager
     }
 
-    fun getCurrentLocation(consumer: Consumer<Location>) {
-//        val provider = if (locationManager.isProviderEnabled(LocationManager.FUSED_PROVIDER)) {
-//            LocationManager.FUSED_PROVIDER
-//        } else if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-//            LocationManager.GPS_PROVIDER
-//        } else {
-//            LocationManager.NETWORK_PROVIDER
-//        }
-//        if (Build.VERSION.SDK_INT >= 30) {
-//            // TODO: probably not the best to create and shutdown an executor every time this runs
-//            val executor = Executors.newSingleThreadExecutor()
-//            locationManager.getCurrentLocation(provider, null, executor) {
-//                executor.shutdown()
-//                consumer.accept(it)
-//            }
-//        } else {
-//            locationManager.requestSingleUpdate(provider, { location ->
-//                consumer.accept(location)
-//            }, null)
-//        }
-
+    fun getCurrentLocation(context: Context, consumer: Consumer<Location?>) {
         val executor = Executors.newSingleThreadExecutor()
+        if (ContextCompat.checkSelfPermission(context, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            consumer.accept(null)
+            return
+        }
+        val provider = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            LocationManager.FUSED_PROVIDER
+        } else {
+            LocationManager.GPS_PROVIDER // TODO: handle unavailable GPS?
+        }
         LocationManagerCompat.getCurrentLocation(
             locationManager,
-            LocationManager.FUSED_PROVIDER,
+            provider,
             null,
             executor
         ) { location ->
