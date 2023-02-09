04597484c3dff16e9ab3f214edc4e7505b1a55b6
Bryce Hostetler <bhostetler@ufl.edu>
2023-02-09 16:01:40 -0500

    Update copyright

--- app/src/main/java/com/uf/biolens/ui/metadata/MetadataViewModel.kt
+++ app/src/main/java/com/uf/biolens/ui/metadata/MetadataViewModel.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 University of Florida
+ * Copyright (c) 2022-2023 University of Florida
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
